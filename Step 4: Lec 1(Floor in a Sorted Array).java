5d69eb0d57e6a49a4945cb8896a36f2119d59949
Endala Harshavardhan <endalaharshavardhan@gmail.com>
2025-03-18 14:06:16 +0530

    Floor in a Sorted Array

---
+++ Step 4: Lec 1(Floor in a Sorted Array).java
@@ -0,0 +1,21 @@
+class Solution {
+
+    static int findFloor(int[] arr, int x) {
+        // write code here
+       int low = 0, high = arr.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
